1b6ac78f8aeec7f8aaf3079737bfccee237eb240
Deep Chothani <chothanideep@gmail.com>
2021-07-28 14:50:36 +0530

    Commit 1

---
+++ GitTokens/src/main/java/com/lti/token/UserApp.java
@@ -0,0 +1,16 @@
+package com.lti.token;
+
+
+public class UserApp {
+
+public static void main(String[] args) {
+		
+		Person p=new Person(11,"ddd",199.99);
+		System.out.println(p);
+		
+	}
+	
+	
+	
+	
+}
